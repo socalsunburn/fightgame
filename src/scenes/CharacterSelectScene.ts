46754fb4d1b89afeb655cb355c605ba449a8a981
Jesse <jessegentile@gmail.com>
2026-05-12 21:27:15 -0700

    Move char select controls text back down 100px

--- src/scenes/CharacterSelectScene.ts
+++ src/scenes/CharacterSelectScene.ts
@@ -87,7 +87,7 @@ export class CharacterSelectScene extends Phaser.Scene {
       this.lockTexts.push(lockText);
     }
 
-    this.add.text(width / 2, height - 144, 'P1: WASD + F/G   |   P2: ARROWS + L/K   |   SHIELD: H / J   |   DOWN = fast fall', {
+    this.add.text(width / 2, height - 44, 'P1: WASD + F/G   |   P2: ARROWS + L/K   |   SHIELD: H / J   |   DOWN = fast fall', {
       fontSize: '16px', color: '#ffd700', fontFamily: 'monospace', fontStyle: 'bold',
       stroke: '#000000', strokeThickness: 3,
     }).setOrigin(0.5);
